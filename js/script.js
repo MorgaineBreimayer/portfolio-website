6749571b4d942355bd09eaa2dc4905f0cc531c08
Morgaine <morgaine.breimayer@gmail.com>
2021-09-03 14:41:58 -0700

    Code cleanup

--- js/script.js
+++ js/script.js
@@ -1,82 +1,78 @@
-var dots = document.getElementById('dot');
-var nav_list = document.getElementById('nav-list');
-var modal = document.getElementById('modal');
-var nav = document.querySelector('.navtoggle');
-var ul = document.querySelector('.navtoggle nav ul');
+var dots = document.getElementById('dot')
+var navList = document.getElementById('nav-list')
+var modal = document.getElementById('modal')
+var nav = document.querySelector('.navtoggle')
+var ul = document.querySelector('.navtoggle nav ul')
 
-
-function menuToggle() {
+function menuToggle () {
   if (dots.className === 'dot') {
-    dots.className += ' clicked';
+    dots.className += ' clicked'
   } else {
-    dots.className = 'dot';
+    dots.className = 'dot'
   }
 
-  if (nav_list.className === 'col-lg-4 col-md-12 nav-list') {
-    nav_list.className += ' clicked';
+  if (navList.className === 'col-lg-4 col-md-12 nav-list') {
+    navList.className += ' clicked'
   } else {
-    nav_list.className = 'col-lg-4 col-md-12 nav-list';
+    navList.className = 'col-lg-4 col-md-12 nav-list'
   }
 
   if (modal.className === 'modal') {
-    modal.className += ' clicked';
+    modal.className += ' clicked'
   } else {
-    modal.className = 'modal';
+    modal.className = 'modal'
   }
 }
 
-
-function menuToggleWork() {
-  dots.className = 'dot';
-  nav_list.className = 'col-lg-4 col-md-12 nav-list';
-  modal.className = 'modal';
+function menuToggleWork () {
+  dots.className = 'dot'
+  navList.className = 'col-lg-4 col-md-12 nav-list'
+  modal.className = 'modal'
 }
 
-
-window.addEventListener("scroll", () => {
-  const currentScroll = window.pageYOffset;
-  var screenWidth = window.width;
+window.addEventListener('scroll', () => {
+  const currentScroll = window.pageYOffset
+  var screenWidth = window.width
 
   if (screenWidth >= 1024) {
-    screenWidth = "large";
+    screenWidth = 'large'
   } else if (screenWidth < 640) {
-    screenWidth = "small";
+    screenWidth = 'small'
   } else {
-    screenWidth = "medium";
+    screenWidth = 'medium'
   }
 
-  if ((currentScroll > 130) && (screenWidth = "large")) {
-    nav.classList.add('scrolled');
+  if ((currentScroll > 130) && (screenWidth = 'large')) {
+    nav.classList.add('scrolled')
   }
-  if ((currentScroll > 195) && (screenWidth = "large")) {
-    nav.classList.add('complete');
+  if ((currentScroll > 195) && (screenWidth = 'large')) {
+    nav.classList.add('complete')
   }
-  if ((currentScroll < 195) && (screenWidth = "large")) {
-    nav.className = 'navtoggle scrolled';
+  if ((currentScroll < 195) && (screenWidth = 'large')) {
+    nav.className = 'navtoggle scrolled'
   }
-  if ((currentScroll < 130) && (screenWidth = "large")) {
-    nav.classList.remove('scrolled');
-    nav.classList.remove('complete');
-    modal.classList.remove('clicked');
-    nav_list.classList.remove('clicked');
-    dots.classList.remove('clicked');
-  }
-
-  if ((currentScroll > 150) && (screenWidth = "medium")) {
-    nav.classList.add('scrolled');
-  }
-  if ((currentScroll > 195) && (screenWidth = "medium")) {
-    nav.classList.add('complete');
-  }
-  if ((currentScroll < 195) && (screenWidth = "medium")) {
-    nav.className = 'navtoggle scrolled';
-  }
-  if ((currentScroll < 150) && (screenWidth = "medium")) {
-    nav.classList.remove('scrolled');
-    nav.classList.remove('complete');
-    modal.classList.remove('clicked');
-    nav_list.classList.remove('clicked');
-    dots.classList.remove('clicked');
+  if ((currentScroll < 130) && (screenWidth = 'large')) {
+    nav.classList.remove('scrolled')
+    nav.classList.remove('complete')
+    modal.classList.remove('clicked')
+    navList.classList.remove('clicked')
+    dots.classList.remove('clicked')
   }
 
-});
+  if ((currentScroll > 150) && (screenWidth = 'medium')) {
+    nav.classList.add('scrolled')
+  }
+  if ((currentScroll > 195) && (screenWidth = 'medium')) {
+    nav.classList.add('complete')
+  }
+  if ((currentScroll < 195) && (screenWidth = 'medium')) {
+    nav.className = 'navtoggle scrolled'
+  }
+  if ((currentScroll < 150) && (screenWidth = 'medium')) {
+    nav.classList.remove('scrolled')
+    nav.classList.remove('complete')
+    modal.classList.remove('clicked')
+    navList.classList.remove('clicked')
+    dots.classList.remove('clicked')
+  }
+})
